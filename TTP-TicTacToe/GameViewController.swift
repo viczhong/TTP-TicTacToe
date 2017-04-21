cdfec7f9e0b5b128346044bf4a7424db714da961
Victor Zhong <viczhong@gmail.com>
2017-04-21 01:26:21 -0400

    Setting up pods and barebones.

---
+++ TTP-TicTacToe/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  TTP-TicTacToe
+//
+//  Created by Victor Zhong on 4/21/17.
+//  Copyright © 2017 Access Code. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TTP-TicTacToe/TicTacToe.swift
@@ -0,0 +1,9 @@
+//
+//  TicTacToe.swift
+//  TTP-TicTacToe
+//
+//  Created by Victor Zhong on 4/21/17.
+//  Copyright © 2017 Access Code. All rights reserved.
+//
+
+import Foundation
